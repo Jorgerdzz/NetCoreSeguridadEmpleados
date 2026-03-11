033dffa981b6644572acefaa4e1dd7b9d00fa03d
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2026-03-11 11:20:58 +0100

    Implementacion de Claims

--- NetCoreSeguridadEmpleados/Controllers/ManagedController.cs
+++ NetCoreSeguridadEmpleados/Controllers/ManagedController.cs
@@ -34,8 +34,22 @@ namespace NetCoreSeguridadEmpleados.Controllers
                         CookieAuthenticationDefaults.AuthenticationScheme,
                         ClaimTypes.Name, ClaimTypes.Role
                         );
+
                 Claim claimName = new Claim(ClaimTypes.Name, username);
                 identity.AddClaim(claimName);
+
+                Claim claimId = new Claim(ClaimTypes.NameIdentifier, empleado.IdEmpleado.ToString());
+                identity.AddClaim(claimId);
+
+                Claim claimRole = new Claim(ClaimTypes.Role, empleado.Oficio);
+                identity.AddClaim(claimRole);
+
+                Claim claimSalario = new Claim("Salario", empleado.Salario.ToString());
+                identity.AddClaim(claimSalario);
+
+                Claim claimDept = new Claim("Departamento", empleado.IdDepartamento.ToString());
+                identity.AddClaim(claimDept);
+
                 //COMO POR AHORA NO VOY A UTILIZAR ROLES, NO LO INDICAMOS
                 ClaimsPrincipal userPrincipal =
                     new ClaimsPrincipal(identity);
